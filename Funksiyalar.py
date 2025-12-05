21c5057e08178836c4dc413f3f44bf7f4c11b546
EXA_GAMER_007 <salimovsarvar518@gmail.com>
2025-12-05 16:30:31 +0500

    suuuu

--- Funksiyalar.py
+++ Funksiyalar.py
@@ -6,29 +6,35 @@
 # print(salomlashish(ism,fam,ota))
 #
 
-print('='*31)
-print('='*9,'KOLKULIATOR','='*9)
-print('='*31)
-
-def kolkulyator(X, Y, amal):
-    if amal == '-':
-        return f"Ayirma: {X-Y}"
-    elif amal == '+':
-        return f"Yig'indi: {X+Y}"
-    elif amal == '*':
-        return f"Ko'oaytma: {X*Y}"
-    elif amal == '/':
-        return f"Bo'linma: {X/Y}"
-    elif amal == '√':
-        return f"Kvadrat ildiz: {X ** 0.5}"
-    elif amal == '²':
-        return f"Kvadrati: {X**Y}"
-    elif amal == '//':
-        return f"Sonlar bo'lindi va butun qismi: {X//Y}"
-    elif amal == '%':
-        return f"Foizi: {X * Y / 100}"
-X = int(input("X sonni kiriting: "))
-Y = int(input("Y sonni kiriting: "))
-amal = input("Amalni kiriting: ")
-
-print(kolkulyator(X,Y,amal))
+# print('='*31)
+# print('='*9,'KOLKULIATOR','='*9)
+# print('='*31)
+#
+# def kolkulyator(X, Y, amal):
+#     if amal == '-':
+#         return f"Ayirma: {X-Y}"
+#     elif amal == '+':
+#         return f"Yig'indi: {X+Y}"
+#     elif amal == '*':
+#         return f"Ko'oaytma: {X*Y}"
+#     elif amal == '/':
+#         return f"Bo'linma: {X/Y}"
+#     elif amal == '√':
+#         return f"Kvadrat ildiz: {X ** 0.5}"
+#     elif amal == '²':
+#         return f"Kvadrati: {X**Y}"
+#     elif amal == '//':
+#         return f"Sonlar bo'lindi va butun qismi: {X//Y}"
+#     elif amal == '%':
+#         return f"Foizi: {X * Y / 100}"
+# X = int(input("X sonni kiriting: "))
+# Y = int(input("Y sonni kiriting: "))
+# amal = input("Amalni kiriting: ")
+#
+# print(kolkulyator(X,Y,amal))
+#
+# print(max(20,10))
+# stringlar = ['python']
+# print(stringlar.reverse())
+# print("assalom".capitalize())
+# #OHIRGISIGA TUSHUNMADIM!
